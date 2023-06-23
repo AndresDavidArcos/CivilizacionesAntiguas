76eea978ef44ca48513d885bc4bbef657e244918
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-06-23 13:41:57 -0500

    Agricultura: Se agregaron pimientos

--- aztec-civilization/src/components/Agricultura.js
+++ aztec-civilization/src/components/Agricultura.js
@@ -10,6 +10,7 @@ import Tomato from './Tomato';
 import Dirt from './Dirt';
 import Corn from './Corn';
 import Pumpkin from './Pumpkin';
+import Pepper from './Pepper';
 import { randInt } from 'three/src/math/MathUtils';
 
 const Agricultura = () => {
@@ -95,67 +96,92 @@ const Agricultura = () => {
       </RigidBody>
 
       {/* Tomato plants */}
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -70]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -70]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -70]} />
-      <Dirt position={[-29, dirt_heigh, -70]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      <group receiveShadow castShadow>
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -70]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -70]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -70]} />
+        <Dirt position={[-29, dirt_heigh, -70]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -63]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -63]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -63]} />
-      <Dirt position={[-29, dirt_heigh, -63]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -63]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -63]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -63]} />
+        <Dirt position={[-29, dirt_heigh, -63]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -56]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -56]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -56]} />
-      <Dirt position={[-29, dirt_heigh, -56]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -56]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -56]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -56]} />
+        <Dirt position={[-29, dirt_heigh, -56]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -49]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -49]} />
-      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -49]} />
-      <Dirt position={[-29, dirt_heigh, -49]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -49]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -49]} />
+        <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -49]} />
+        <Dirt position={[-29, dirt_heigh, -49]} scale={[0.7, 0.2, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      </group>
 
       {/* Corn plants */}
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -35]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -35]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -35]} />
-      <Dirt position={[-29, dirt_heigh, -35]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      <group receiveShadow castShadow>
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -35]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -35]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -35]} />
+        <Dirt position={[-29, dirt_heigh, -35]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -28]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -28]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -28]} />
-      <Dirt position={[-29, dirt_heigh, -28]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -28]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -28]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -28]} />
+        <Dirt position={[-29, dirt_heigh, -28]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -20]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -20]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -20]} />
-      <Dirt position={[-29, dirt_heigh, -20]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -20]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -20]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -20]} />
+        <Dirt position={[-29, dirt_heigh, -20]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -12]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -12]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -12]} />
-      <Dirt position={[-29, dirt_heigh, -12]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -12]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -12]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -12]} />
+        <Dirt position={[-29, dirt_heigh, -12]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -4]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -4]} />
-      <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -4]} />
-      <Dirt position={[-29, dirt_heigh, -4]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-22, 2, -4]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-29, 2, -4]} />
+        <Corn rotation={[0, (angleToRadians(randInt(0, 360))), 0]} position={[-36, 2, -4]} />
+        <Dirt position={[-29, dirt_heigh, -4]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      </group>
 
       {/* Pumpkin plants */}
-      <Pumpkin position={[-22, 2.5, 8]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Pumpkin position={[-29, 2.5, 8]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Pumpkin position={[-36, 2.5, 8]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Dirt position={[-29, dirt_heigh, 8]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      <group receiveShadow castShadow>
 
-      <Pumpkin position={[-22, 2.5, 18]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Pumpkin position={[-29, 2.5, 18]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Pumpkin position={[-36, 2.5, 18]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Dirt position={[-29, dirt_heigh, 18]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Pumpkin position={[-22, 2.5, 8]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pumpkin position={[-29, 2.5, 8]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pumpkin position={[-36, 2.5, 8]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Dirt position={[-29, dirt_heigh, 8]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
 
-      <Pumpkin position={[-22, 2.5, 28]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Pumpkin position={[-29, 2.5, 28]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Pumpkin position={[-36, 2.5, 28]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
-      <Dirt position={[-29, dirt_heigh, 28]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+        <Pumpkin position={[-22, 2.5, 18]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pumpkin position={[-29, 2.5, 18]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pumpkin position={[-36, 2.5, 18]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Dirt position={[-29, dirt_heigh, 18]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+
+        <Pumpkin position={[-22, 2.5, 28]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pumpkin position={[-29, 2.5, 28]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pumpkin position={[-36, 2.5, 28]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Dirt position={[-29, dirt_heigh, 28]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      </group>
+
+      {/* Pepper plants */}
+      <group receiveShadow castShadow>
+        <Pepper position={[-22, 2.5, 42]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pepper position={[-29, 2.5, 42]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pepper position={[-36, 2.5, 42]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Dirt position={[-29, dirt_heigh, 42]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+
+        <Pepper position={[-22, 2.5, 52]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pepper position={[-29, 2.5, 52]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pepper position={[-36, 2.5, 52]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Dirt position={[-29, dirt_heigh, 52]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+
+        <Pepper position={[-22, 2.5, 62]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pepper position={[-29, 2.5, 62]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Pepper position={[-36, 2.5, 62]} rotation={[0, (angleToRadians(randInt(0, 360))), 0]} />
+        <Dirt position={[-29, dirt_heigh, 62]} scale={[0.7, 0.1, 0.9]} rotation={[0, (angleToRadians(90)), 0]} />
+      </group>
 
       {/*Right crops fences  */}
       <RigidBody type="fixed" colliders="cuboid">
