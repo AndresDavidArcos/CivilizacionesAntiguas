818c0bdf336573c08782ed5339436492e48fee75
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-06-22 23:28:37 -0500

    Tomato: Se agregaron plantas de tomate

--- aztec-civilization/src/components/Agricultura.js
+++ aztec-civilization/src/components/Agricultura.js
@@ -6,122 +6,141 @@ import Boat from './Boat';
 import Player from './Player';
 import { Physics, RigidBody } from '@react-three/rapier';
 import BoatPlayer from './BoatPlayer';
+import Tomato from './Tomato';
+import { randInt } from 'three/src/math/MathUtils';
 
 const Agricultura = () => {
   const envMap = useEnvironment({ files: "../../ambientes/industrial_sunset_puresky_1k.hdr" })
 
   return (<>
-            {/* Testing Camera */}
-          {/* <PerspectiveCamera makeDefault position={[-10, 5, 0]} />
+    {/* Testing Camera */}
+    {/* <PerspectiveCamera makeDefault position={[-10, 5, 0]} />
           <CameraControls/> */}
-          <ambientLight args={["#ffffff", 0.25]}/>
+    <ambientLight args={["#ffffff", 0.25]} />
 
     {/* Camera */}
     <PointerLockControls />
 
-      <Physics>
-        <KeyboardControls
-          map={[
-            { name: "forward", keys: ["ArrowUp", "w", "W"] },
-            { name: "backward", keys: ["ArrowDown", "s", "S"] },
-            { name: "left", keys: ["ArrowLeft", "a", "A"] },
-            { name: "right", keys: ["ArrowRight", "d", "D"] },
-          ]}>
-          <BoatPlayer position={[0,1,0]} scale={[9,9,9]}/>
-        </KeyboardControls>
+    <Physics>
+      <KeyboardControls
+        map={[
+          { name: "forward", keys: ["ArrowUp", "w", "W"] },
+          { name: "backward", keys: ["ArrowDown", "s", "S"] },
+          { name: "left", keys: ["ArrowLeft", "a", "A"] },
+          { name: "right", keys: ["ArrowRight", "d", "D"] },
+        ]}>
+        <BoatPlayer position={[0, 1, 0]} scale={[9, 9, 9]} />
+      </KeyboardControls>
 
-            {/* floor */}
-            <RigidBody type="fixed" colliders="cuboid">
-            <mesh rotation={[-(angleToRadians(90)), 0, 0]} receiveShadow>
-                    <planeGeometry args={[120, 180]} />
-                    <meshStandardMaterial color="#54B5CA"  metalness={1} roughness={0.2}/>
-                </mesh>
-            </RigidBody>
-            {/* Floatin Crop */}
-            <RigidBody type="fixed" colliders="cuboid">
-                <mesh rotation={[-(angleToRadians(90)), 0, 0]} position={[-30,1,0]} receiveShadow>
-                    <boxGeometry args={[30, 160]} />
-                    <meshStandardMaterial color="#C19A6B" />
-                </mesh>
-            </RigidBody>
-              <RigidBody type="fixed" colliders="cuboid">
+      {/* floor */}
+      <RigidBody type="fixed" colliders="cuboid">
+        <mesh rotation={[-(angleToRadians(90)), 0, 0]} receiveShadow>
+          <planeGeometry args={[120, 180]} />
+          <meshStandardMaterial color="#54B5CA" metalness={1} roughness={0.2} />
+        </mesh>
+      </RigidBody>
+      {/* Floatin Crop */}
+      <RigidBody type="fixed" colliders="cuboid">
+        <mesh rotation={[-(angleToRadians(90)), 0, 0]} position={[-30, 1, 0]} receiveShadow>
+          <boxGeometry args={[30, 160]} />
+          <meshStandardMaterial color="#C19A6B" />
+        </mesh>
+      </RigidBody>
+      <RigidBody type="fixed" colliders="cuboid">
 
-                <mesh rotation={[-(angleToRadians(90)), 0, 0]} position={[30,1,0]} receiveShadow>
-                    <boxGeometry args={[30, 160]} />
-                    <meshStandardMaterial color="#C19A6B" />
-                </mesh> 
-                </RigidBody>
+        <mesh rotation={[-(angleToRadians(90)), 0, 0]} position={[30, 1, 0]} receiveShadow>
+          <boxGeometry args={[30, 160]} />
+          <meshStandardMaterial color="#C19A6B" />
+        </mesh>
+      </RigidBody>
 
-                {/* Boat */}
-                {/* <Boat position={[0,1,0]} scale={[9,9,9]}/> */}
-                  {/*Left crops fences  */}
-                <RigidBody type="fixed" colliders="cuboid">                  
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,-45]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,-36]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,-18]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,0]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,18]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,36]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,54]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,72]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44,2,90]} scale={[6,6,6]} receiveShadow/>
-                </RigidBody>
+      {/* Boat */}
+      {/* <Boat position={[0,1,0]} scale={[9,9,9]}/> */}
+      {/*Left crops fences  */}
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, -45]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, -36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, -18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, 0]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, 18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, 36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, 54]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, 72]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-44, 2, 90]} scale={[6, 6, 6]} receiveShadow />
+      </RigidBody>
 
-                <RigidBody type="fixed" colliders="cuboid">
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,-45]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,-36]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,-18]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,0]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,18]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,36]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,54]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,72]} scale={[6,6,6]} receiveShadow/>
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15,2,90]} scale={[6,6,6]} receiveShadow/>
-                </RigidBody>
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, -45]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, -36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, -18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, 0]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, 18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, 36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, 54]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, 72]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[15, 2, 90]} scale={[6, 6, 6]} receiveShadow />
+      </RigidBody>
 
-                <RigidBody type="fixed" colliders="cuboid">                  
-                <Fence rotation={[0, 0, 0]} position={[-62,2,75]} scale={[8,6,6]} receiveShadow/>
-                </RigidBody>
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, 0, 0]} position={[-62, 2, 75]} scale={[8, 6, 6]} receiveShadow />
+      </RigidBody>
 
-                <RigidBody type="fixed" colliders="cuboid">                  
-                <Fence rotation={[0, 0, 0]} position={[-62,2,-75]} scale={[8,6,6]} receiveShadow/>
-                </RigidBody>
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, 0, 0]} position={[-62, 2, -75]} scale={[8, 6, 6]} receiveShadow />
+      </RigidBody>
 
-                {/*Right crops fences  */}
-                <RigidBody type="fixed" colliders="cuboid">
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, -45]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, -36]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, -18]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 0]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 18]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 36]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 54]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 72]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 90]} scale={[6, 6, 6]} receiveShadow />
-              </RigidBody>
-              <RigidBody type="fixed" colliders="cuboid">
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, -45]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, -36]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, -18]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 0]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 18]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 36]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 54]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 72]} scale={[6, 6, 6]} receiveShadow />
-                <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 90]} scale={[6, 6, 6]} receiveShadow />
-              </RigidBody>
+      {/* Tomato plants */}
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -70]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -70]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -70]} />
 
-              <RigidBody type="fixed" colliders="cuboid">                  
-                <Fence rotation={[0, 0, 0]} position={[-3,2,75]} scale={[8,6,6]} receiveShadow/>
-                </RigidBody>
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -63]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -63]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -63]} />
 
-                <RigidBody type="fixed" colliders="cuboid">                  
-                <Fence rotation={[0, 0, 0]} position={[-3,2,-75]} scale={[8,6,6]} receiveShadow/>
-                </RigidBody>
-                </Physics>
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -56]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -56]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -56]} />
 
-      {/* Enviroments */}
-      <Environment map={envMap} background />     
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-22, 2, -49]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-29, 2, -49]} />
+      <Tomato rotation={[0, (angleToRadians(randInt(-90, 90))), 0]} position={[-36, 2, -49]} />
+
+      {/*Right crops fences  */}
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, -45]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, -36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, -18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 0]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 54]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 72]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[44, 2, 90]} scale={[6, 6, 6]} receiveShadow />
+      </RigidBody>
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, -45]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, -36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, -18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 0]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 18]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 36]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 54]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 72]} scale={[6, 6, 6]} receiveShadow />
+        <Fence rotation={[0, (angleToRadians(90)), 0]} position={[-15, 2, 90]} scale={[6, 6, 6]} receiveShadow />
+      </RigidBody>
+
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, 0, 0]} position={[-3, 2, 75]} scale={[8, 6, 6]} receiveShadow />
+      </RigidBody>
+
+      <RigidBody type="fixed" colliders="cuboid">
+        <Fence rotation={[0, 0, 0]} position={[-3, 2, -75]} scale={[8, 6, 6]} receiveShadow />
+      </RigidBody>
+    </Physics>
+
+    {/* Enviroments */}
+    <Environment map={envMap} background />
 
   </>);
 };
